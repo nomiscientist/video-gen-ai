b5fcb96d8fec0b1da24516a2cfa287ccd23490b9
Sham <7894324+nomiscientist@users.noreply.github.com>
2023-12-04 16:20:04 +0000

    text

--- biden.py
+++ biden.py
@@ -1,28 +1,28 @@
 
-# import torch
-# import torchaudio
-# import torch.nn as nn
-# import torch.nn.functional as F
+import torch
+import torchaudio
+import torch.nn as nn
+import torch.nn.functional as F
 
-# #import IPython
+#import IPython
 
-# from tortoise.api import TextToSpeech
-# from tortoise.utils.audio import load_audio, load_voice, load_voices
+from tortoise.api import TextToSpeech
+from tortoise.utils.audio import load_audio, load_voice, load_voices
 
-# from LIHQ.procedures.tortoise_scripts import tortoise_run, tortoise_combo_run
+from LIHQ.procedures.tortoise_scripts import tortoise_run, tortoise_combo_run
 
-# tts = TextToSpeech()
+tts = TextToSpeech()
 
 
 # Generating a random voice to speak the sentence below. When you generate one you like, save it using the cell below
 
-# text = "Hello, I'm Morgan Freeman, and I support this message. Thank you, and may God bless the United States of America."
-# voice = 'freeman'
-# preset = "high_quality" #Options: {"ultra_fast", "fast", "standard", "high_quality"}.
+text = "Hello, I'm Morgan Freeman, and I support Refusion AI."
+voice = 'freeman'
+preset = "high_quality" #Options: {"ultra_fast", "fast", "standard", "high_quality"}.
 
-# gen = tortoise_run(tts, text, voice, preset)
+gen = tortoise_run(tts, text, voice, preset)
 
-# torchaudio.save('LIHQ/input/audio/Folder1/2.wav', gen.squeeze(0).cpu(), 24000)
+torchaudio.save('LIHQ/input/audio/Folder1/2.wav', gen.squeeze(0).cpu(), 24000)
 
 
 # Adding half a second of silence before audio, because FOMM works better when first frame is a closed mouth.
