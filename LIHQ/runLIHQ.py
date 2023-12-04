be9d10259603c3720b98cbffb13e84edd71316aa
Sham <7894324+nomiscientist@users.noreply.github.com>
2023-12-04 02:11:11 +0000

    path ch

--- LIHQ/runLIHQ.py
+++ LIHQ/runLIHQ.py
@@ -71,7 +71,7 @@ def run(face, audio_super = 'LIHQ/input/audio/', ref_vid = 'LIHQ/input/ref_vid/s
     print("Running Wav2Lip")
     for adir in aud_dir_names:
         wav2lip_run(adir)
-    w2l_folders = sorted(glob.glob('./LIHQ/output/wav2Lip/*'))
+    w2l_folders = sorted(glob.glob('/home/ec2-user/extras/LIHQ/output/wav2Lip/*'))
     print(w2l_folders)
     if len(w2l_folders) < len(aud_dir_names):
         print('Wav2Lip could not generate at least one of your videos.\n'
